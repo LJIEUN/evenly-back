663d49ebc8a5214f59e3cf7d06714889c6503b4d
LJIEUN <eun773c@daum.net>
2025-03-17 17:10:48 +0900

    feat: 상품 목록 엔티티 추가

---
+++ src/main/java/com/codeisevenlycooked/evenly/entity/Product.java
@@ -0,0 +1,48 @@
+package com.codeisevenlycooked.evenly.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "product")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private int price;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private String imageUrl;
+
+    @Column(nullable = false)
+    private String category;
+
+    @Column(nullable = false)
+    private int stock;
+
+    @Column(nullable = false)
+    private String status;
+
+    @Column(nullable = false)
+    @CreationTimestamp
+    private Timestamp created_at;
+
+}
