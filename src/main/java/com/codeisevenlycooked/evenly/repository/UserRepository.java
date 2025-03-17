84efd6935ac9581cb7601b97e7dea166d453afbf
nahyun <knahyuk@gmail.com>
2025-03-17 14:31:50 +0900

    feat: user repository

--- src/main/java/com/codeisevenlycooked/evenly/repository/UserRepository.java
+++ src/main/java/com/codeisevenlycooked/evenly/repository/UserRepository.java
@@ -1,4 +1,10 @@
 package com.codeisevenlycooked.evenly.repository;
 
-public interface UserRepository {
+import com.codeisevenlycooked.evenly.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    // userId 중복체크
+    boolean existsByUserId(String userId);
+
 }
