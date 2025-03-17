1038e4705939ef27e0e32f7a12881e09d319ae5b
LJIEUN <eun773c@daum.net>
2025-03-18 01:56:02 +0900

    feat: 상품 목록 Controller 추가_v1 (상품 목록 조회)

---
+++ src/main/java/com/codeisevenlycooked/evenly/controller/ProductController.java
@@ -0,0 +1,27 @@
+package com.codeisevenlycooked.evenly.controller;
+
+import com.codeisevenlycooked.evenly.entity.Product;
+import com.codeisevenlycooked.evenly.repository.ProductRepository;
+import com.codeisevenlycooked.evenly.service.ProductService;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+@RequiredArgsConstructor
+public class ProductController {
+    private final ProductService productService;
+
+    //상품 목록 조회
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
+    //상품 상세 조회 ( = 단일 상품 조회 )
+}
