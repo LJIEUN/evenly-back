6ea5469cc9aa031881f3113a4d242fcebdba5555
nahyun <knahyuk@gmail.com>
2025-03-17 14:39:26 +0900

    feat: 회원가입 dto

--- src/main/java/com/codeisevenlycooked/evenly/dto/SignUpDto.java
+++ src/main/java/com/codeisevenlycooked/evenly/dto/SignUpDto.java
@@ -1,4 +1,25 @@
 package com.codeisevenlycooked.evenly.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
+
+@Getter
 public class SignUpDto {
+
+    @NotBlank(message = "아이디는 필수 입력 값입니다.")
+    private String userId;
+
+    @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
+    @Pattern(
+            regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[!@#$%^&*])[A-Za-z\\d!@#$%^&*]{8,}$",
+            message = "비밀번호는 8자 이상이며, 대문자, 소문자, 숫자, 특수문자를 포함해야 합니다."
+    )
+    private String password;
+
+    @NotBlank(message = "이름은 필수 입력 값입니다.")
+    private String name;
+
+
+
 }
