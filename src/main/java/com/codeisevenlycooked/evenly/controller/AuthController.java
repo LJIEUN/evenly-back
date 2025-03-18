a8f318f744009aa83a81bf626b9d4957818798a4
nahyun <knahyuk@gmail.com>
2025-03-18 18:22:15 +0900

    feat: 토큰 만료시 refresh API(체크 및 수정 필요)

--- src/main/java/com/codeisevenlycooked/evenly/controller/AuthController.java
+++ src/main/java/com/codeisevenlycooked/evenly/controller/AuthController.java
@@ -1,8 +1,11 @@
 package com.codeisevenlycooked.evenly.controller;
 
+import com.codeisevenlycooked.evenly.config.security.JwtUtil;
 import com.codeisevenlycooked.evenly.dto.SignInDto;
 import com.codeisevenlycooked.evenly.dto.SignUpDto;
 import com.codeisevenlycooked.evenly.service.AuthService;
+import io.jsonwebtoken.JwtException;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -18,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthController {
     private final AuthService authService;
+    private final JwtUtil jwtUtil;
 
     @PostMapping("/signup")
     public ResponseEntity<String> signUp(@Valid @RequestBody SignUpDto signUpDto) {
@@ -32,6 +36,21 @@ public class AuthController {
                 .header("Authorization", "Bearer " + tokens[0])
                 .header("Refresh-Token", tokens[1])
                 .body("로그인 성공!");
+    }
+
+    @PostMapping("/refresh")
+    public ResponseEntity<String> refreshAccessToken(HttpServletRequest request) {
+        String refreshToken = jwtUtil.resolveToken(request);
+
+        if (refreshToken == null || refreshToken.isBlank()) {
+            return ResponseEntity.badRequest().body("{\"message\": \"Refresh Token is missing\"}");
+        }
+        try {
+            String newAccessToken = jwtUtil.renewAccessToken(refreshToken);
+            return ResponseEntity.ok(newAccessToken);
+        } catch (JwtException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("{\"message\": \"Invalid Token\"}");
+        }
     }
 
 }
