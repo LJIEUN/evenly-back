c322d205dbf9687a9ffa230f912ca540e609afa3
nahyun <knahyuk@gmail.com>
2025-03-18 18:36:24 +0900

    config: Redis 서비스 추가

--- src/main/java/com/codeisevenlycooked/evenly/config/security/SecurityConfig.java
+++ src/main/java/com/codeisevenlycooked/evenly/config/security/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.codeisevenlycooked.evenly.config.security;
 
 import com.codeisevenlycooked.evenly.global.filter.JwtAuthenticationFilter;
+import com.codeisevenlycooked.evenly.service.RedisService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,6 +17,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @RequiredArgsConstructor
 public class SecurityConfig {
     private final JwtUtil jwtUtil;
+    private final RedisService redisService;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -27,7 +29,7 @@ public class SecurityConfig {
                     .requestMatchers("/", "/products/**").permitAll()// 홈화면(상품 목록), 상품 상세, 등 추가 필요
                     .anyRequest().authenticated() // 그외 모든 요청은 인증 필요
             )
-            .addFilterBefore(new JwtAuthenticationFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class);
+            .addFilterBefore(new JwtAuthenticationFilter(jwtUtil, redisService), UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
