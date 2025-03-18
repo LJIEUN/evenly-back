2a34917546fac2a94db04846c5715b25faf29b8b
nahyun <knahyuk@gmail.com>
2025-03-18 11:35:46 +0900

    feat: signin service 추가

--- src/main/java/com/codeisevenlycooked/evenly/repository/UserRepository.java
+++ src/main/java/com/codeisevenlycooked/evenly/repository/UserRepository.java
@@ -3,7 +3,10 @@ package com.codeisevenlycooked.evenly.repository;
 import com.codeisevenlycooked.evenly.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUserId(String userId);
     // userId 중복체크
     boolean existsByUserId(String userId);
 
