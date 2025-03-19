ea0ebe8ffcb3a36c8ac63de15f7d0fd7d81b66de
LJIEUN <eun773c@daum.net>
2025-03-19 12:11:27 +0900

    fix: DTO 추가로 인한 Controller 수정

--- src/main/java/com/codeisevenlycooked/evenly/controller/ProductController.java
+++ src/main/java/com/codeisevenlycooked/evenly/controller/ProductController.java
@@ -1,10 +1,12 @@
 package com.codeisevenlycooked.evenly.controller;
 
+import com.codeisevenlycooked.evenly.dto.ProductResponseDto;
 import com.codeisevenlycooked.evenly.entity.Product;
 import com.codeisevenlycooked.evenly.repository.ProductRepository;
 import com.codeisevenlycooked.evenly.service.ProductService;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,13 +22,13 @@ public class ProductController {
 
     //상품 목록 조회
     @GetMapping
-    public List<Product> getAllProducts() {
-        return productService.getAllProducts();
+    public ResponseEntity<List<ProductResponseDto>> getAllProducts() {
+        return ResponseEntity.ok(productService.getAllProducts());
     }
 
     //상품 상세 조회 ( = 단일 상품 조회 )
     @GetMapping("/{id}")
-    public Product getProductById(@PathVariable Long id) {
-        return productService.getProductById(id);
+    public ResponseEntity<ProductResponseDto> getProductById(@PathVariable Long id) {
+        return ResponseEntity.ok(productService.getProductById(id));
     }
 }
