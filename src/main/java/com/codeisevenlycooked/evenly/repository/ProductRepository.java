e674e7de838f4906ff3341b3e641c83d9080a42e
LJIEUN <eun773c@daum.net>
2025-03-19 12:08:42 +0900

    fix: DTO 추가로 인한 Repository 수정

--- src/main/java/com/codeisevenlycooked/evenly/repository/ProductRepository.java
+++ src/main/java/com/codeisevenlycooked/evenly/repository/ProductRepository.java
@@ -1,7 +1,12 @@
 package com.codeisevenlycooked.evenly.repository;
 
 import com.codeisevenlycooked.evenly.entity.Product;
+import com.codeisevenlycooked.evenly.entity.ProductStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Collection;
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findByStatusNot(ProductStatus status);
 }
