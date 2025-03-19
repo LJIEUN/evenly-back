f8be400603864295ba8cb695d11024a6ff403caa
LJIEUN <eun773c@daum.net>
2025-03-19 12:08:01 +0900

    fix: DTO 추가로 인한 Servie 수정

--- src/main/java/com/codeisevenlycooked/evenly/service/ProductService.java
+++ src/main/java/com/codeisevenlycooked/evenly/service/ProductService.java
@@ -1,6 +1,8 @@
 package com.codeisevenlycooked.evenly.service;
 
+import com.codeisevenlycooked.evenly.dto.ProductResponseDto;
 import com.codeisevenlycooked.evenly.entity.Product;
+import com.codeisevenlycooked.evenly.entity.ProductStatus;
 import com.codeisevenlycooked.evenly.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,13 +15,16 @@ public class ProductService {
     private final ProductRepository productRepository;
 
     //상품 목록 조회 부분
-    public List<Product> getAllProducts() {
-        return productRepository.findAll();
+    public List<ProductResponseDto> getAllProducts() {
+        return productRepository.findByStatusNot(ProductStatus.DELETED).stream()
+                .map(ProductResponseDto::new)
+                .toList();
     }
 
     //상품 상세 조회 부분 ( = 단일 상품 조회)
-    public Product getProductById(Long id) {
-        return productRepository.findById(id)
+    public ProductResponseDto getProductById(Long id) {
+         Product product = productRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("상품을 찾을 수 없습니다."));
+         return new ProductResponseDto(product);
     }
 }
