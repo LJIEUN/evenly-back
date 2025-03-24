4a2bd921ac78f465f4b5eceac6ec69593a64e668
LJIEUN <eun773c@daum.net>
2025-03-24 11:19:55 +0900

    feat: 장바구니아이템 entitiy 추가

---
+++ src/main/java/com/codeisevenlycooked/evenly/entity/CartItem.java
@@ -0,0 +1,38 @@
+package com.codeisevenlycooked.evenly.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "cart_items")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CartItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "cart_id", nullable = false)
+    private Cart cart;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @Column(nullable = false)
+    private int quantity;
+
+    public int totalPrice() {
+        return product.getPrice() * quantity;
+    }
+
+
+}
