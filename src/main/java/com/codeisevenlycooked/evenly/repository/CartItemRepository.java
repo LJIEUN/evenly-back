1f99ad18ddd3263ca4386c14c684d807092e4b2b
LJIEUN <eun773c@daum.net>
2025-03-24 15:06:06 +0900

    feat: CartItemRepository 상품 추가시 필요한 메서드 추가

--- src/main/java/com/codeisevenlycooked/evenly/repository/CartItemRepository.java
+++ src/main/java/com/codeisevenlycooked/evenly/repository/CartItemRepository.java
@@ -2,10 +2,15 @@ package com.codeisevenlycooked.evenly.repository;
 
 import com.codeisevenlycooked.evenly.entity.Cart;
 import com.codeisevenlycooked.evenly.entity.CartItem;
+import com.codeisevenlycooked.evenly.entity.Product;
+import com.codeisevenlycooked.evenly.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
     List<CartItem> findByCart(Cart cart);
+
+    Optional<CartItem> findByCartAndProduct(Cart cart, Product product);
 }
