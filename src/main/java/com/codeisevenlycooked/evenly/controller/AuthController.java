5e42fd6f273716b3118be71dc8e119618778f3f9
nahyun <knahyuk@gmail.com>
2025-03-25 22:42:12 +0900

    fix: 토큰 재발급 API 응답 json으로 변경

--- src/main/java/com/codeisevenlycooked/evenly/controller/AuthController.java
+++ src/main/java/com/codeisevenlycooked/evenly/controller/AuthController.java
@@ -14,6 +14,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
+
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
@@ -36,17 +38,17 @@ public class AuthController {
     }
 
     @PostMapping("/refresh")
-    public ResponseEntity<String> refreshAccessToken(HttpServletRequest request) {
+    public ResponseEntity<Map<String, String>> refreshAccessToken(HttpServletRequest request) {
         String refreshToken = jwtUtil.resolveToken(request);
 
         if (refreshToken == null || refreshToken.isBlank()) {
-            return ResponseEntity.badRequest().body("{\"message\": \"Refresh Token is missing\"}");
+            return ResponseEntity.badRequest().body(Map.of("messsage", "Refresh Token is missing"));
         }
         try {
             String newAccessToken = jwtUtil.renewAccessToken(refreshToken);
-            return ResponseEntity.ok(newAccessToken);
+            return ResponseEntity.ok(Map.of("accessToken", newAccessToken));
         } catch (JwtException e) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("{\"message\": \"Invalid Token\"}");
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(Map.of("messsage", "Invalid Token"));
         }
     }
 
