42799c4d8c88353fdd147fd2e3e3218a98bbfcf8
nahyun <knahyuk@gmail.com>
2025-03-25 18:33:11 +0900

    feat: 예외처리

--- src/main/java/com/codeisevenlycooked/evenly/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/codeisevenlycooked/evenly/global/exception/GlobalExceptionHandler.java
@@ -1,7 +1,11 @@
 package com.codeisevenlycooked.evenly.global.exception;
 
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -11,5 +15,28 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public ResponseEntity<String> handleJsonParseError(HttpMessageNotReadableException ex) {
         return ResponseEntity.badRequest().body("400 Bad Request: 올바르지 않은 JSON 형식입니다.");
+    }
+    // id, password 형식 등
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        FieldError fieldError = ex.getBindingResult().getFieldError();
+        String message = fieldError != null ? fieldError.getDefaultMessage() : "유효성 검사 오류";
+
+        return new ResponseEntity<>(new ErrorResponse(message), HttpStatus.BAD_REQUEST);
+    }
+    // ID 중복, 사용자 없음, 비밀번호 틀림, 회원 삭제 후 로그인 등
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<Object> handleIllegalArgumentException(IllegalArgumentException ex) {
+        return new ResponseEntity<>(new ErrorResponse(ex.getMessage()), HttpStatus.BAD_REQUEST);
+    }
+    // 재결제 시도 등
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<Object> handleRuntimeException(RuntimeException ex) {
+        return new ResponseEntity<>(new ErrorResponse(ex.getMessage()), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> handleException(Exception ex){
+            return new ResponseEntity<>(new ErrorResponse("서버 오류가 발생했습니다."), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
