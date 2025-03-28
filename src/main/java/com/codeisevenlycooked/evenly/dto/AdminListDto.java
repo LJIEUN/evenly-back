d5d1a41644627205d98c7d6b5fe55ad2f496b5ed
LJIEUN <eun773c@daum.net>
2025-03-28 13:19:45 +0900

    ref: AdminListDto Order관련 리팩토링

--- src/main/java/com/codeisevenlycooked/evenly/dto/AdminListDto.java
+++ src/main/java/com/codeisevenlycooked/evenly/dto/AdminListDto.java
@@ -16,4 +16,30 @@ public class AdminListDto {
     private BigDecimal totalPrice;
     private OrderStatus status;
     private LocalDateTime createAt;
+
+    private String receiverName;
+    private String address;
+    private String mobile;
+    private String deliveryMessage;
+    private PaymentMethod paymentMethod;
+
+    private List<OrderItem> orderItems;
+
+    public AdminListDto(Order order) {
+        this.id = order.getId();
+        this.orderNumber = order.getOrderNumber();
+        this.userId = order.getUser().getUserId();
+        this.totalPrice = order.getTotalPrice();
+        this.status = order.getStatus();
+        this.createAt = order.getCreatedAt();
+
+        this.receiverName = order.getReceiverName();
+        this.address = order.getAddress();
+        this.mobile = order.getMobile();
+        this.deliveryMessage = order.getDeliveryMessage();
+        this.paymentMethod = order.getPaymentMethod();
+        this.orderItems = order.getOrderItems();
+    }
+
+
 }
